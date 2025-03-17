8ab993da9431f693e17788a434c6bdeb859db5e3
Mgclaman <amansinghgurjar810@gmail.com>
2025-03-18 00:54:31 +0530

    Add files via upload

---
+++ Soil Moisture Detector System.ino
@@ -0,0 +1,34 @@
+/* ****** ||                                     Created By Aman Sing Gurjar|| ****** 
+
+
+ #include <LiquidCrystal.h>
+LiquidCrystal lcd(A0,A1,A2,A3,A4,A5 );//****
+#define soil 9
+void setup() {
+ pinMode(soil, INPUT);
+pinMode(10, OUTPUT);
+ lcd.begin(16, 2);
+ lcd.print(" AUTOMATIC ");
+ lcd.setCursor(0,1);
+ lcd.print("IRRIGATION SYSTEM");
+ delay(2000);
+}
+void loop() {
+ if(digitalRead(soil)==HIGH)
+ {
+ digitalWrite(10,HIGH);
+ // delay(200);
+ lcd.clear();
+ lcd.print(" MOTOR");
+ lcd.setCursor(0, 1);
+ lcd.print(" ON ");
+ delay(300);
+ }
+ else {
+ digitalWrite(10, LOW);
+ //digitalWrite(fire, LOW);
+ lcd.clear();
+ lcd.print(" MOTOR ");
+ lcd.setCursor(0,1);
+ lcd.print(" OFF ");
+ delay(200);
